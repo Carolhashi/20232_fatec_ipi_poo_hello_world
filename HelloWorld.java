49fb6566b4202d36d541175e94a291d948a0edc0
Carolina Hashimoto <carolina.hashimoto@fatec.sp.gov.br>
2023-08-31 15:54:58 -0300

    ap01(java): hello world
    - Utilizando linha de comando

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+    public static void main(String [] args){
+        //stdin stdout
+        System.out.println("Hello, World");
+    }
+}
+//Em Java, o compilador produz bytecode
+//é a linguagem que JVM entende
+
+//A JVM interpreta bytecode
