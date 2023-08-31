ebac49b9a5c2a1a4b82d25b66c510b63c5b13882
Carolina Hashimoto <carolina.hashimoto@fatec.sp.gov.br>
2023-08-31 16:03:55 -0300

    ap01(java): hello HelloWorld
    - Usando JOptionPane

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,11 @@
+//pacote padrao java,lang
+//importado automaticamente
+import javax.swing.JOptionPane;
 public class HelloWorld{
     public static void main(String [] args){
-        //stdin stdout
-        System.out.println("Hello, World");
+        //stdin stdout stderr
+        //System.out.println("Hello, World");
+        JOptionPane.showMessageDialog(null, "Hello, World");
     }
 }
 //Em Java, o compilador produz bytecode
